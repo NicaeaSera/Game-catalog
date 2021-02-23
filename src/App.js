97c45b76a10566dd5008af6ae90d0153931be374
Nicaea Sera <lizagod2000@mail.ru>
2021-02-23 13:53:32 +0300

    updated "sign up page", added "registration" page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,47 +11,111 @@ import Home from "./views/Home/Home.js";
 import Profile from "./views/Profile/Profile.js";
 import Sign from "./views/Sign/Sign.js";
 import Admin from "./views/Admin/Admin.js";
+import Registration from "./views/Registration/Registration.js";
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Button color="primary">Hello World</Button>
-              <Link to="/">Home</Link>  
-            </li>
-            <li>
-              <Link to="/catalog">Catalog</Link>
-            </li>
-            <li>
-              <Link to="/sign">Sign in/ Sign up</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/admin">Admin panel</Link>
-            </li>
-          </ul>
-        </nav>
 
-        <Switch>
-          <Route path="/profile">
+
+if(localStorage.isAuthorised != "NOT_LOGGED_IN" && localStorage.isAuthorised !="LOGGED_IN"){
+  localStorage.isAuthorised = "NOT_LOGGED_IN";
+  localStorage.login = "";
+  localStorage.password = "";
+  localStorage.logLink = "/log_in";
+  localStorage.logMsg = "Log in";
+}
+if(localStorage.login === undefined && localStorage.login ===""){
+  localStorage.isAuthorised = "NOT_LOGGED_IN";
+  localStorage.login = "";
+  localStorage.password = "";
+  localStorage.logLink = "/log_in";
+  localStorage.logMsg = "Log in";
+}
+
+function LogOut(){
+  localStorage.isAuthorised = "NOT_LOGGED_IN";
+  localStorage.login = "";
+  localStorage.password = "";
+  localStorage.logLink = "/log_in";
+  localStorage.logMsg = "Log in";
+  window.location.href = "/"
+}
+
+export default class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      logInStatus: localStorage.isAuthorised,
+      user: localStorage.user,
+      logLink: localStorage.logLink,
+      logMsg: localStorage.logMsg
+    };
+  } 
+
+  logOut = () => {
+    this.setState({
+      logInStatus: "NOT_LOGGED_IN",
+      user: {},
+      logLink: "/sdd",
+      logMsg: "Log out"
+    })
+  }
+
+  UpdateLogStatus = () =>{
+    if(localStorage.isAuthorised === "LOGGED_IN"){
+      this.setState({
+        logInStatus: "LOGGED_IN",
+        user: {login: localStorage.login},
+        logLink: "log_out",
+        logMsg: "Log out"
+      })
+    }
+  }
+  render(){
+    
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Button color="primary" href="/">Main page</Button>
+              </li>
+              <li>
+                <Button color="primary" href="/catalog">Catalog</Button>
+              </li>
+              <li>
+                <Button color="primary" href={this.state.logLink}>{this.state.logMsg}" "{this.state.logInStatus}" "{this.state.logLink}</Button>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/admin">Admin panel</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/profile">
               <Profile />
             </Route>
-            <Route path="/sign">
+            <Route path="/log_in">
               <Sign />
+            </Route>
+            <Route path="/registration">
+              <Registration />
             </Route>
             <Route path="/admin">
               <Admin />
             </Route>
+            <Route path="/log_out">
+              <LogOut />
+            </Route>
             <Route path="/">
               <Home />
             </Route> 
-        </Switch>
-      </div>
-    </Router>
-  );
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
--- src/views/Registration/Registration.js
+++ src/views/Registration/Registration.js
@@ -14,47 +14,83 @@ import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
 import { AirlineSeatReclineExtraOutlined } from '@material-ui/icons'
 
-
-function sendAuthorisationRequest(login, password){
-    fetch('http://localhost:3000/login', {
-    method: 'POST',
-    body: JSON.stringify({ email: login, password: password }),
-  headers: {
-    'Content-Type': 'application/json'
+function sendRegistrationRequest(login, password, confirm_password){
+  if(login === ""){
+    alert("Incorrect mail");
+    return
   }
-})
-.then(response => 
+  if(password != confirm_password){
+    alert("Password do not match");
+    window.location.href = "/registration";
+    return
+  }
+
+  let logarr = Array.from(login);
+  let tempA = Array.from(password);
+  if(tempA.length < 4){
+    alert("password lenght must be atleast 4");    
+    window.location.href = "/registration";
+    return
+  }
+  tempA ="";
+  for(let i = 0; i < logarr.length; i += 1){
+    if(logarr[i] === "@"){
+      if(tempA === logarr[i]){
+        alert("incorrect login");
+        tempA = "";
+        window.location.href = "/registration";
+        return
+      }
+      tempA = logarr[i];
+    }
+  };
+
+  if(login != "" && password === confirm_password && tempA === "@"){
+    fetch('http://localhost:3000/register', {
+      method: 'POST',
+      body: JSON.stringify({ email: login, password: password }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })      
+  .then(response => 
     {
-      console.log(response);
       if(response.ok){
-        alert("welcome back!");
-        localStorage.setItem("login", login);
-        localStorage.setItem("isAuthorised", "LOGGED_IN");
-        localStorage.setItem("password", password);
-        localStorage.setItem("logLink", "/log_out");
-        localStorage.setItem("logMsg", "Log Out");        
-        window.location.href = "/"
+        alert("Success!");
+        window.location.href = "/log_in";
         return
       }
       if(!response.ok){
-        alert("incorrect username or password");
+        alert("incorrect login");
+        window.location.href = "/registration";
         return
       }
-    })
+      window.location.href = "/registration";  
+      alert("error");
+    })        
+  }
+  window.location.href = "/registration";
+  return
+  
+  
+  
+  
 }
 
-
-class Sign extends React.Component {
+class Registration extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       login: "",
-      password: ""};
+      password: "",
+      confirm_password: ""
+    };
   }
   
  
   handleSubmit = (event) => {
     event.preventDefault()
+
     const data = new FormData(event.target)
     const value = Object.fromEntries(data.entries())
     console.log({ value })
@@ -81,7 +117,7 @@ class Sign extends React.Component {
           <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-          Sign in
+            Welcome
           </Typography>
             <form className={classes.form} noValidate id="form">
           <TextField
@@ -96,7 +132,7 @@ class Sign extends React.Component {
             autoFocus
             onChange={() => this.setState({login: document.getElementById("email").value})}
           />
-          <p>TEST {this.state.login}  {this.state.password}</p>
+          <p>TEST{this.state.login}  {this.state.password} {this.state.confirm_password}</p>
           <TextField
             variant="outlined"
             margin="normal"
@@ -106,8 +142,18 @@ class Sign extends React.Component {
             label="Password"
             type="password"
             id="password"
-            autoComplete="current-password"
             onChange={() => this.setState({password: document.getElementById("password").value})}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="confirm_password"
+            label="Confirm password"
+            type="password"
+            id="confirm_password"
+            onChange={() => this.setState({confirm_password: document.getElementById("confirm_password").value})}
           />
           <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
@@ -119,9 +165,9 @@ class Sign extends React.Component {
             variant="contained"
             color="primary"
             className={classes.submit}
-            onClick={() => sendAuthorisationRequest(this.state.login, this.state.password)}
+            onClick={() => sendRegistrationRequest(this.state.login, this.state.password, this.state.confirm_password)}
           >
-            Sign In
+            Sign up
           </Button>
           <Grid container>
             <Grid item xs>
@@ -177,4 +223,4 @@ const styles = theme => ({
   },
 })
 
-export default withStyles(styles)(Sign)
+export default withStyles(styles)(Registration)
