8f733bf8af05420c3fab784002954b82d3f80260
Nicaea Sera <lizagod2000@mail.ru>
2021-03-10 15:42:31 +0300

    some test changes

--- src/views/Api/GamesApi.js
+++ src/views/Api/GamesApi.js
@@ -1,3 +1,4 @@
+import { Games } from '@material-ui/icons';
 import { baseUrl, defaultHeaders } from './Api'
 
 const entityUrl = 'games'
@@ -7,26 +8,26 @@ const baseRequestSettings = {
   cache: 'default'
 }
 
-// запрос на игры по-фильру
-const filter = (page = 1, size = 10, filterParams = {}) => {
+// запрос на игры по-фильтру
+const filter = (page = 1, size = 12, filterParams = {}) => {
   // генерируем ссылку
-  const url = `${baseUrl + entityUrl}?_page=${page}&_limit=${size}`
-
+  const url = `${baseUrl + entityUrl}?_page=${page}&_limit=${size}`;
   //если переданы доп. параметры для фильтрации -- дополняем ссылку
   if (Object.keys(filterParams).length > 0) {
     Object.keys(filterParams).forEach(key => {
       url += `&${key}=${filterParams[key]}`
     });
   }
-
   //выполняем запрос и возвращаем promise-запроса
   return fetch(url, {
     method: 'GET',
     ...baseRequestSettings
   }).then(response => {
+    //console.log(response.headers.get("Link"));
+    //let navLinks = parseData(response.headers.get("Link"));  
     return response.json().then(data => ({
       Data: data, // данные что пришли с сервера
-      Count: response.headers.get('X-Total-Count') // общее кол-во карточек, нужно для пагинации
+      Count: response.headers.get('X-Total-Count'), // общее кол-во карточек, нужно для пагинации
     }))
   })
 }
